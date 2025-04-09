b45f02a7f2926c0646b3f8796786ddae48d8f8d3
Michael Godwin <mickeygodwin07@gmail.com>
2025-04-09 21:27:39 +0000

    mobile responsiveness on header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,49 +1,41 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import './Header.css'
+import { Link } from 'react-router-dom';
 import logo from '../../assets/MyLogo.png';
+import './Header.css'; // Assuming the styles are saved in Header.css
 
 const Header = () => {
-  const location = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
-    <div className='MainHeader'>
-        <div className="subHeader">
-            <div className="logo">
-              <Link to="/">
-                <img src={logo} alt="Logo" />
-              </Link>            
-            </div>
-            <div className="nav">
-            <Link 
-            to="/about" 
-            className={location.pathname === '/about' ? 'active' : ''}
-          >
-            About me
+    <div className="MainHeader">
+      <div className="subHeader">
+        <div className="logo">
+          <Link to="/" className="nav-link">
+            <img src={logo} alt="Logo" />
           </Link>
-          <Link 
-            to="/contact" 
-            className={location.pathname === '/contact' ? 'active' : ''}
-          >
-            Contact me
-          </Link>
-          <Link 
-            to="/projects" 
-            className={location.pathname === '/projects' ? 'active' : ''}
-          >
-            My Projects
-          </Link>
-          <Link 
-            to="/resume" 
-            className={location.pathname === '/resume' ? 'active' : ''}
-          >
-            Resume/CV
-          </Link>
-            </div>
         </div>
-      
-    </div>
-  )
-}
 
-export default Header
+        {/* Hamburger menu */}
+        <div className="hamburger" onClick={toggleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        {/* Navigation links */}
+        <div className={`nav ${menuOpen ? 'active' : ''}`}>
+          <Link to="/about" className="nav-link">About</Link>
+          <Link to="/contact" className="nav-link">Contact</Link>
+          <Link to="/projects" className="nav-link">Projects</Link>
+          <Link to="/resume" className="nav-link">Resume</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
